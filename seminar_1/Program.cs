38023d393dc90410f4e6aefff4ff22e7e591f5e0
Zakhozhij <maxzahojii@mail.ru>
2022-10-03 10:36:10 +0300

    added results

--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите a: ");
+string a = Console.ReadLine() ?? "";
+Console.Write("Введите b: ");
+string b = Console.ReadLine() ?? "";
+int int_a = int.Parse(a);
+int int_b = int.Parse(b);
+if(int_a == (int_b * int_b)){
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
+Console.WriteLine("--------------------");
+Console.Write("Введите день недели: ");
+string weekday = Console.ReadLine() ?? "";
+int int_weekday = int.Parse(weekday);
+if(int_weekday >= 1 && int_weekday <= 7){
+    if(int_weekday == 1){
+        Console.WriteLine(int_weekday + " -> Понедельник");
+    }else if(int_weekday == 2){
+        Console.WriteLine(int_weekday + " -> Вторник");
+    }else if(int_weekday == 3){
+        Console.WriteLine(int_weekday + " -> Среда");
+    }else if(int_weekday == 4){
+        Console.WriteLine(int_weekday + " -> Четверг");
+    }else if(int_weekday == 5){
+        Console.WriteLine(int_weekday + " -> Пятница");
+    }else if(int_weekday == 6){
+        Console.WriteLine(int_weekday + " -> Суббота");
+    }else if(int_weekday == 7){
+        Console.WriteLine(int_weekday + " -> Воскресенье");
+    }
+    
+}else{
+    Console.WriteLine("Вышли за диапозон дней или что-то не то");
+}
---
+++ seminar_dz_1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+//Task 1
+Console.WriteLine("Задача 1 (максимальное из двух чисел)");
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int max = a;
+if(b > max){
+    max = b;
+}
+Console.WriteLine("Число a = " + a);
+Console.WriteLine("Число b = " + b);
+Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine("------------------------------");
+//Task 2
+Console.WriteLine("Задача 2 (максимальное из трех чисел)");
+a = new Random().Next(1, 10);
+b = new Random().Next(1, 10);
+int c = new Random().Next(1, 10);
+max = a;
+if(b > max){
+    max = b;
+}
+if(c > max){
+    max = c;
+}
+Console.WriteLine("Число a = " + a);
+Console.WriteLine("Число b = " + b);
+Console.WriteLine("Число c = " + c);
+Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine("------------------------------");
+//Task 3
+Console.WriteLine("Задача 3 (четное ли число)");
+a = new Random().Next(-100, 100);
+Console.WriteLine("Число a = " + a);
+if((a % 2) == 0){
+    Console.WriteLine("Четное");
+}else{
+    Console.WriteLine("Не четное");
+}
+Console.WriteLine("------------------------------");
+//Task 4
+Console.WriteLine("Задача 4 (четные числа в промежутке)");
+Console.Write("Введите число N: ");
+string end = Console.ReadLine() ?? "";
+int N = int.Parse(end);
+Console.Write("В промежутке от 1 до " + N + " следующие четные числа: ");
+int start = 1;
+while(start <= N){
+    if((start % 2) == 0){
+        Console.Write(start + " ");
+    }
+    start++;
+}
