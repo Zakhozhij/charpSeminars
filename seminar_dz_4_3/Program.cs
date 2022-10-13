d7b282db117638021e2e4d28062f7bd62736fa15
Zakhozhij <maxzahojii@mail.ru>
2022-10-13 09:32:28 +0300

    Added seminar 4 dz (edited 1 task)

--- seminar_dz_4_3/Program.cs
+++ seminar_dz_4_3/Program.cs
@@ -1,4 +1,5 @@
-﻿void FillArray(int[] inputsArr){
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+void FillArray(int[] inputsArr){
     int length = inputsArr.Length;
     int index = 0;
     for(int i = 0; i<length; i++){
