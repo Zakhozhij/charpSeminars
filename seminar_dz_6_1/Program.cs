e5a6176b3057a3bb4066d60e813637fa43b55de6
Zakhozhij <maxzahojii@mail.ru>
2022-10-19 11:13:31 +0300

    Added seminar dz 6

---
+++ seminar_dz_6_1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+void FillArray(int[] inputsArr){
+    int length = inputsArr.Length;
+    for(int i = 0; i<length; i++){
+        Console.Write($"Введите {i+1} элемент массива -> ");
+        inputsArr[i]=int.Parse(Console.ReadLine() ?? "");
+    }
+}
+int CountPositive(int[] inputsArr){
+    int results = 0;
+    foreach (int val in inputsArr){
+        if(val > 0){
+            results++;
+        }
+    }
+    return results;
+}
+
+Console.Write("Введите размер массива: ");
+int size;
+try
+{
+    size=int.Parse(Console.ReadLine() ?? "");
+}catch(Exception exc){
+    Console.WriteLine($"Ошибка ввода данных!!!! {exc.Message}");
+    return;
+}
+int[] array = new int[size];
+FillArray(array);
+int CountPositiveValues = CountPositive(array);
+
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> {CountPositiveValues}");
---
+++ seminar_dz_6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.Clear();
+double inputValues(string Message){
+    Console.Write(Message);
+    double value=double.Parse(Console.ReadLine() ?? "");
+    return value;
+}
+double k1 = inputValues("Введите k1: ");
+double b1 = inputValues("Введите b1: ");
+double k2 = inputValues("Введите k2: ");
+double b2 = inputValues("Введите b2: ");
+if(k1 - k2 == 0){
+    Console.WriteLine($"k1 не может равняться k2!");
+}else{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения двух прямых -> ({x};{y})");
+}
+
+
+
